2db8e6324f95fb504f9cdff42b949030aac1be2e
quangnckh25dev-gif <quangnckh25.dev@gmail.com>
2026-06-06 15:37:10 +0700

    first commit

---
+++ src/main/java/com/quangnv/horseracing/HorseRacingBeApplication.java
@@ -0,0 +1,13 @@
+package com.quangnv.horseracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseRacingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorseRacingBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quangnv/horseracing/HorseRacingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quangnv.horseracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseRacingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
